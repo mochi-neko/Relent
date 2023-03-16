7f96670f23cf460852660153def5558fde936b19
mochi-neko <t.o.e.4315@gmail.com>
2023-03-16 21:19:39 +0900

    Add samples

--- Assets/Mochineko/HttpResult.Samples/HttpClientSample.cs
+++ Assets/Mochineko/HttpResult.Samples/HttpClientSample.cs
@@ -13,15 +13,13 @@ namespace Mochineko.HttpResult.Samples
     [TestFixture]
     internal class HttpClientSample
     {
-        private static readonly HttpClient httpClient
-            = new HttpClient();
-
         [Test]
         [RequiresPlayMode(false)]
         public async Task GetSample()
         {
             var url = "https://www.google.com/";
             using var cancellationTokenSource = new CancellationTokenSource();
+            using var httpClient = new HttpClient();
             var result = await CallGetAPIAsync(httpClient, url, cancellationTokenSource.Token);
             if (result is IHttpSuccessResult<string> success)
             {
@@ -31,12 +29,12 @@ namespace Mochineko.HttpResult.Samples
             else if (result is IHttpRetryableResult<string> retryable)
             {
                 // Can retry
-                Debug.LogWarning(retryable.Message);
+                throw new Exception();
             }
             else if (result is IHttpFailureResult<string> failure)
             {
                 // Handle error
-                Debug.LogError(failure.Message);
+                throw new Exception();
             }
             else
             {
@@ -44,18 +42,89 @@ namespace Mochineko.HttpResult.Samples
                 throw new Exception();
             }
         }
-      
+
+        [Test]
+        [RequiresPlayMode(false)]
+        public async Task CancelByUser()
+        {
+            LogAssert.ignoreFailingMessages = true;
+            
+            using var cancellationTokenSource = new CancellationTokenSource();
+            cancellationTokenSource.CancelAfter(TimeSpan.FromSeconds(0.1d));
+            
+            var result = await WaitAsync(cancellationTokenSource.Token, TimeSpan.FromSeconds(10d));
+            if (result is IHttpSuccessResult<string> success)
+            {
+                // Happy path
+                throw new Exception();
+            }
+            else if (result is IHttpRetryableResult<string> retryable)
+            {
+                // Can retry
+                throw new Exception();
+            }
+            else if (result is IHttpFailureResult<string> failure)
+            {
+                // Handle error
+            }
+            else
+            {
+                // Unexpected
+                throw new Exception();
+            }
+        }
+        
+        [Test]
+        [RequiresPlayMode(false)]
+        public async Task CancelByTimeout()
+        {
+            LogAssert.ignoreFailingMessages = true;
+            
+            using var httpClient = new HttpClient();
+            httpClient.Timeout = TimeSpan.FromSeconds(0.1d);
+            
+            var url = "https://www.google.com/";
+            
+            var retryCount = 0;
+
+            while (retryCount < 5)
+            {
+                var result = await CallGetAPIAsync(
+                    httpClient,
+                    url,
+                    CancellationToken.None);
+                if (result is IHttpSuccessResult<string> success)
+                {
+                    // Happy path
+                    throw new Exception("Succeeded is invalid");
+                }
+                else if (result is IHttpRetryableResult<string> retryable)
+                {
+                    // Can retry
+                    retryCount++;
+                    Debug.Log($"Retry:{retryCount}");
+                }
+                else if (result is IHttpFailureResult<string> failure)
+                {
+                    // Handle error
+                    throw new Exception(failure.Message);
+                }
+                else
+                {
+                    // Unexpected
+                    throw new Exception();
+                }
+            }
+            
+            Debug.Log("Over retry count.");
+            GC.KeepAlive(httpClient);
+        }
+
         private static async Task<IHttpResult<string>> CallGetAPIAsync(
             HttpClient httpClient,
             string url,
             CancellationToken cancellationToken)
         {
-            if (cancellationToken.IsCancellationRequested)
-            {
-                return HttpResult.Fail<string>(
-                    $"Failed because task has been already cancelled when called the API.");
-            }
-
             using var requestMessage = new HttpRequestMessage(HttpMethod.Get, url);
             try
             {
@@ -66,7 +135,7 @@ namespace Mochineko.HttpResult.Samples
                     return HttpResult.Fail<string>(
                         $"Failed because {nameof(HttpResponseMessage)} was null.");
                 }
-                
+
                 if (responseMessage.IsSuccessStatusCode)
                 {
                     if (responseMessage.Content == null)
@@ -89,6 +158,8 @@ namespace Mochineko.HttpResult.Samples
                 else if (responseMessage.StatusCode is HttpStatusCode.TooManyRequests
                          || (int)responseMessage.StatusCode is >= 500 and <= 599)
                 {
+                    Debug.LogWarning(
+                        $"Retryable due to [{(int)responseMessage.StatusCode}:{responseMessage.StatusCode}].");
                     return HttpResult.Retry<string>(
                         $"Retryable due to [{(int)responseMessage.StatusCode}:{responseMessage.StatusCode}].");
                 }
@@ -105,28 +176,79 @@ namespace Mochineko.HttpResult.Samples
             // Request error
             catch (HttpRequestException exception)
             {
+                Debug.LogWarning(
+                    $"Retryable because {nameof(HttpRequestException)} was thrown during calling the API:{exception}.");
                 return HttpResult.Retry<string>(
                     $"Retryable because {nameof(HttpRequestException)} was thrown during calling the API:{exception}.");
             }
             // User cancellation
             catch (TaskCanceledException exception)
-                when (cancellationToken.IsCancellationRequested)
+                when (exception.CancellationToken == cancellationToken)
             {
+                Debug.LogError(
+                    $"Failed because task was canceled by user during call to the API:{exception}.");
                 return HttpResult.Fail<string>(
                     $"Failed because task was canceled by user during call to the API:{exception}.");
+            }
+            // Timeout or other cancellation
+            catch (TaskCanceledException exception)
+            {
+                Debug.LogWarning(
+                    $"Retryable because task was cancelled during calling the API:{exception}.");
+                return HttpResult.Retry<string>(
+                    $"Retryable because task was cancelled during calling the API:{exception}.");
+            }
+            // Timeout or other cancellation 
+            catch (OperationCanceledException exception)
+            {
+                Debug.LogWarning(
+                    $"Retryable because operation was cancelled during calling the API:{exception}.");
+                return HttpResult.Retry<string>(
+                    $"Retryable because operation was cancelled during calling the API:{exception}.");
+            }
+            // Unhandled error
+            catch (Exception exception)
+            {
+                Debug.LogError(
+                    $"Failed because an unhandled exception was thrown when calling the API:{exception}.");
+                return HttpResult.Fail<string>(
+                    $"Failed because an unhandled exception was thrown when calling the API:{exception}.");
+            }
+        }
+
+        private static async Task<IHttpResult<string>> WaitAsync(
+            CancellationToken cancellationToken,
+            TimeSpan time)
+        {
+            try
+            {
+                await Task.Delay(time, cancellationToken);
+
+                Debug.Log("Finished");
+                return HttpResult.Succeed("Finished");
+            }
+            // User cancellation
+            catch (TaskCanceledException exception)
+                when (cancellationToken.IsCancellationRequested)
+            {
+                Debug.LogError(exception);
+                return HttpResult.Fail<string>(
+                    $"Failed because task was canceled by user during waiting with exception:{exception}.");
             }
             // Timeout cancellation
             catch (TaskCanceledException exception)
                 when (exception.InnerException is TimeoutException)
             {
+                Debug.LogWarning(exception);
                 return HttpResult.Retry<string>(
-                    $"Retryable because {nameof(TimeoutException)} was thrown during calling the API:{exception}.");
+                    $"Retryable because timeout during waiting with exception:{exception}.");
             }
             // Unhandled error
             catch (Exception exception)
             {
+                Debug.LogException(exception);
                 return HttpResult.Fail<string>(
-                    $"Failed because an unhandled exception was thrown when calling the API:{exception}.");
+                    $"Failed because an unhandled exception was thrown when waiting:{exception}.");
             }
         }
     }
---
+++ Assets/Mochineko/HttpResult.Samples/UserDefinesResultSample.cs
@@ -0,0 +1,57 @@
+#nullable enable
+using System;
+using FluentAssertions;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+namespace Mochineko.HttpResult.Samples
+{
+    [TestFixture]
+    internal class UserDefinesResultSample
+    {
+        [Test]
+        [RequiresPlayMode(false)]
+        public void Sample()
+        {
+            IHttpResult<string> result = new MyHttpFailureResult<string>("Test", "000");
+
+            result.Success.Should().BeFalse();
+            result.Retryable.Should().BeFalse();
+            result.Failure.Should().BeTrue();
+
+            if (result is IHttpSuccessResult<string>)
+            {
+                throw new Exception();
+            }
+            else if (result is IHttpRetryableResult<string>)
+            {
+                throw new Exception();
+            }
+            else if (result is MyHttpFailureResult<string> myFailure)
+            {
+                myFailure.Message.Should().Be("Test");
+                myFailure.ErrorCode.Should().Be("000");
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+        
+        internal sealed class MyHttpFailureResult<TResult>
+            : IHttpFailureResult<TResult>
+        {
+            public bool Success => false;
+            public bool Retryable => false;
+            public bool Failure => true;
+            public string Message { get; }
+            public string ErrorCode { get; }
+
+            public MyHttpFailureResult(string message, string errorCode)
+            {
+                Message = message;
+                ErrorCode = errorCode;
+            }
+        }
+    }
+}
